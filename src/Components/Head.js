2397e22bfdb6a8fcfcf3be26f8d5b483875318d6
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-06-19 17:07:16 +0900

    1 file modified

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
 
 const Head = () => {
-  return <div>Head</div>;
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <MenuRoundedIcon className="h-8" />
+        <img
+          className="h-8 mx-2"
+          alt="yt-logo"
+          src="https://www.gstatic.com/youtube/img/branding/youtubelogo/svg/youtubelogo.svg"
+        />
+      </div>
+      <div className="col-span-10 text-center">
+        <input
+          type="text"
+          className="w-1/2 border border-grey-400 p-2 rounded-l-full"
+        ></input>
+        <button className="border border-grey-400 p-2 rounded-r-full">
+          Search
+        </button>
+      </div>
+
+      <div>
+        <PersonOutlineRoundedIcon className="h-8" />
+      </div>
+    </div>
+  );
 };
 
 export default Head;
