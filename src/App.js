c899c05464d3425f342bdbaee9778b2b816138ad
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-06-21 08:58:22 +0900

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import './style.css';
+import store from './Utils/store';
 import Head from './Components/Head';
 import Body from './Components/Body';
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <div>
         <Head />
         <Body />
