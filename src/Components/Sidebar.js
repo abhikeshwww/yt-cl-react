4a3b523c81e20440812b45ea7d7aaf20447efb9d
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-06-26 09:02:20 +0900

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,36 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import store from '../Utils/store';
+import React from "react";
+import { useSelector } from "react-redux"; 
+import { Link } from "react-router-dom";
 
-const Sidebar = () => {
-  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
+const SideBar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
   if(!isMenuOpen) return null;
-  //early return
+  
   return (
-    <div className="p-5 shadow-lg w-48">
-      <ul>
-        <li>Home</li>
-        <li>Shorts</li>
-        <li>Videos</li>
-        <li>Live</li>
-      </ul>
-      <h1 className="font-bold  pt-5">Subscriptions</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-
-      <h1 className="font-bold pt-5">Watch Later</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
+    <div className="p-5 shadow-lg w-48 ">
+      <ul >
+        <Link to={'/'}><li>Home</li> </Link>
+        <li>Shorts</li> 
+        <li>SubSciptions</li> 
+        <li>Originals</li>
+        <li>Youtube Music</li>
+        <li>Playlist</li>
       </ul>
     </div>
   );
 };
-
-export default Sidebar;
+export default SideBar
