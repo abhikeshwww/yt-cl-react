e830947930f070d83fab30d7f0efdde97ce13b69
abhikeshwww <137013271+abhikeshwww@users.noreply.github.com>
2023-06-26 09:02:41 +0900

    Update VideoContainer.js

--- src/Components/VideoContainer.js
+++ src/Components/VideoContainer.js
@@ -1,7 +1,30 @@
-import React from 'react';
+ import React, { useEffect, useState } from 'react'
+import { YOUTUBE_API } from '../util/Constants';
+import VideoCard, { AdCardHigherOrderComponent } from './VideoCard';
+import { Link } from 'react-router-dom';
+import VideoCntSidebar from './VideoCntSidebar';
+export const VideoContainer = () => {
+  const [videos , setVideos] = useState([]);
+  useEffect(()=> {
+    getVideos();
+  }, []);
 
-const VideoContainer = () => {
-  return <div>VideoContainer</div>;
-};
+  const getVideos = async ()=>{
+    const data = await fetch(YOUTUBE_API);
+    const json = await data.json();
+    setVideos(json.items);
+    console.log(videos)
+  }
 
-export default VideoContainer;
+
+  return (<>
+    <div className='flex flex-wrap'>
+      {videos[0] && <AdCardHigherOrderComponent info={videos[0]}/>}
+      {videos.map((video)=>(
+        <Link to={"watch?v="+video.id} ><VideoCard key = {video.id} info= {video}/></Link> )
+      )}
+        </div>
+        
+       </>
+  )
+}
